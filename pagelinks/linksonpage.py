f1ab2c60d6f7928e1e6be45c33889deecb3b137d
rry0 <ryosec@proton.me>
2024-04-25 12:49:37 +1000

    Tools

---
+++ pagelinks/linksonpage.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+url = "https://"
+
+response = requests.get(url)
+html = response.text
+soup = BeautifulSoup(html, "html.parser")
+links = soup.findAll("a")
+for link in links:
+    print(link.get("href"))
+
+# Open a file in write mode
+with open("pagelinks.txt", "w") as file:
+    # Iterate over links and write them to the file
+    for link in links:
+        file.write(str(link.get("href")) + "\n")
