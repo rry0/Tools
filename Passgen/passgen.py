8dc6ae0cef43ea225ce775dbb8fc7a83afa95c78
ryo <160228468+rry0@users.noreply.github.com>
2024-07-06 13:04:15 +1000

    updated readability

--- Passgen/passgen.py
+++ Passgen/passgen.py
@@ -1,15 +1,26 @@
 import random
 
-print(" ")
-amount = int(input("Enter the amount of passwords to generate: "))
-print(" ")
+# Character pool to create passwords from
 chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=[]{}|;:'\",.<>?/\\"
 
+print(" ") # Gap for readability in terminal
+
+amount = int(input("Enter the amount of passwords to generate: "))
+
+print(" ") # Gap for readability in terminal
+
+# Function to create passwords
 for a in range(amount):
     password = ""
+
     length = int(input("Enter password length: "))
-    print(" ")
+
+    print(" ") # Gap for readability in terminal
+
     for c in range(length):
-        password += random.choice(chars)
+
+        password += random.choice(chars) 
+
     print(password)
-    print(" ")
+    
+    print(" ") # Gap for readability in terminal
